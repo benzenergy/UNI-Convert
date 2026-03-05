8a7c74870496650f8dcd538239836e397dfe6bc1
benzenergy <161916636+benzenergy@users.noreply.github.com>
2026-03-05 15:25:55 +0400

    Add files via upload

---
+++ src/AboutWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * UNI Convert
+ * Copyright (C) 2026 benzenergy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace ver10
+{
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+        }
+    }
+}
---
+++ src/AppSettings.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * UNI Convert
+ * Copyright (C) 2026 benzenergy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ver10;
+
+public class AppSettings
+{
+    public SeasonSettings Summer { get; set; } = new();
+    public SeasonSettings Winter { get; set; } = new();
+}
+
+public class SeasonSettings
+{
+    public double PropCity { get; set; }
+    public double PropHighway { get; set; }
+    public double RateCity { get; set; }
+    public double RateHighway { get; set; }
+}
