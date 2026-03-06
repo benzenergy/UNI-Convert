d67572e62acd8f0f03d7e9defc6ffacf51a55387
benzenergy <161916636+benzenergy@users.noreply.github.com>
2026-03-06 10:58:45 +0400

    Add files via upload

---
+++ src/DarkMessageBox.xaml.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * UNI Convert
+ * Copyright (C) 2026 benzenergy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Windows;
+
+namespace uniconvert
+{
+    public partial class DarkMessageBox : Window
+    {
+        public DarkMessageBox(string message)
+        {
+            InitializeComponent();
+            MessageText.Text = message;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        public static void Show(string message, Window owner)
+        {
+            var box = new DarkMessageBox(message);
+            box.Owner = owner;
+            box.ShowDialog();
+        }
+    }
+}
